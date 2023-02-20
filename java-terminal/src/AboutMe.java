b7fb4eb86bc40c038a9f43da1435a6b95782ac28
FranklinProtazio <franklinprotazio@gmail.com>
2023-02-20 08:08:37 -0300

    Ultimos Exercicios

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,11 @@
+
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+    
+        int i1 = 10; float f1 = 4.5f; char c1 = 'x';
+        boolean b1 = true;
+        
+    }
+}
--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,6 +1,7 @@
 import java.security.PublicKey;
 
 public class SmartTv {
+    
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
